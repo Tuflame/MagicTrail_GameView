f65669c705f7fcc3eac14747aa14f36d786c650b
Kevin <kevin931110@gmail.com>
2025-06-27 11:37:10 +0800

    fix ui

--- src/component/game/Log.tsx
+++ src/component/game/Log.tsx
@@ -12,7 +12,7 @@ export function Log({ logs }: { logs: GameLog[] }) {
         {logs.map((log, index) => (
           <div key={index} className="Log-row">
             <span className="Log-time">
-              {log.round} {log.phase} :
+              {log.round} {log.phase} 
             </span>
             <span className="Log-message"> {log.message}</span>
           </div>
